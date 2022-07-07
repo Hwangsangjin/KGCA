14c2aadd09957076cc87bb3eaef5c52dcfbe11de
상진 <ghkdtkdwls88@gmail.com>
2022-07-07 21:43:18 +0900

    학원에서 커밋

--- C/main.c
+++ C/main.c
@@ -2,6 +2,24 @@
 
 int main()
 {
+	char str[100] = { 0 };
+	int count = 0;
+
+	printf("문장 입력 : ");
+	gets(str);
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (isupper(str[i]))
+		{
+			str[i] = tolower(str[i]);
+			count++;
+		}
+	}
+
+	printf("바뀐 문장 : ");
+	puts(str);
+	printf("바뀐 문자 수 : %d\n", count);
 
 	return 0;
 }
