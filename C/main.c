ef0f56d36097dea34a1b1a32bd3f1621ff3e406b
상진 <ghkdtkdwls88@gmail.com>
2022-07-09 22:51:19 +0900

    집에서 커밋

--- C/main.c
+++ C/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
@@ -7,7 +8,7 @@ int main()
 
 	printf("공백이 포함된 문자열 입력 : ");
 	fgets(str, _countof(str), stdin);
-
+	str[strlen(str) - 1] = '\0';
 	printf("입력된 문자열은 %s입니다.\n", str);
 
 	return 0;
