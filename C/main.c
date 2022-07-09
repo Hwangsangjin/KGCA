557ef1bc1774d4d4cba82e77fc0d2d3e1493fb42
상진 <ghkdtkdwls88@gmail.com>
2022-07-10 00:40:58 +0900

    집에서 커밋

--- C/main.c
+++ C/main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main()
 {
-	char str[80];
+	char str1[80] = "strawberry";
+	char str2[80] = "apple";
+	char* ps1 = "banana";
+	char* ps2 = str2;
 
-	printf("공백이 포함된 문자열 입력 : ");
-	fgets(str, _countof(str), stdin);
-	str[strlen(str) - 1] = '\0';
-	printf("입력된 문자열은 %s입니다.\n", str);
+	printf("최초 문자열 : %s\n", str1);
+	strcpy(str1, str2);
+	printf("바뀐 문자열 : %s\n", str1);
+
+	strcpy(str1, ps1);
+	printf("바뀐 문자열 : %s\n", str1);
+
+	strcpy(str1, "banana");
+	printf("바뀐 문자열 : %s\n", str1);
 
 	return 0;
 }
