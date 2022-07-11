d0960b8196c12397bace6fea8298f8a9c3339d70
상진 <ghkdtkdwls88@gmail.com>
2022-07-11 21:50:33 +0900

    학원에서 커밋

--- C/main.c
+++ C/main.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+int main(int argc, char** argv)
 {
-	char mark[5][5] = { 0 };
-
-	for (size_t i = 0; i < _countof(mark); i++)
+	for (size_t i = 0; i < argc; i++)
 	{
-		for (size_t j = 0; j < _countof(mark); j++)
-		{
-			if ((i == j) || (i + j == 4))
-			{
-				mark[i][j] = 'X';
-				printf("%c", mark[i][j]);
-			}
-			else
-			{
-				mark[i][j] = 'O';
-				printf("%c", mark[i][j]);
-			}
-		}
-		printf("\n");
+		printf("%s\n", argv[i]);
 	}
 
 	return 0;
