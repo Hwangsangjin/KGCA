0e5d34101f6b33f0f2a8b95d8eb17a5d35cbd223
상진 <ghkdtkdwls88@gmail.com>
2022-07-15 15:18:02 +0900

    학원에서 커밋

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,98 +1,32 @@
 #include <iostream>
 using namespace std;
 
-struct Stack
+int main()
 {
-	int size;
-	int top;
-	int* data;
-};
+	char str[80];
+	char ch;
+	int i = 0;
 
-void Init(Stack* ps, int size);
-void Push(Stack* ps, int num);
-int Pop(Stack* ps);
-void Delete(Stack* ps);
+	// get() 함수를 사용한 한 줄 출력
+	cout << "문장 입력 : ";
 
-int main(void)
-{
-	Stack stack;
-	int sel, num;
+	ch = cin.get();		// 공백 문자를 포함한 모든 문자의 입력
 
-	Init(&stack, 5);
-
-	while (1)
+	while (ch != '\n')	// 새줄 문자가 아닌 동안 반복
 	{
-		cout << "> 1.추가, 2.삭제, 0.종료 : ";
-		cin >> sel;
-
-		if (sel == 0)
-		{
-			break;
-		}
-
-		switch (sel)
-		{
-		case 1:
-			cout << "> 번호 입력 : ";
-			cin >> num;
-			Push(&stack, num);
-			break;
-		case 2:
-			num = Pop(&stack);
-			cout << "> 삭제 번호 " << num << endl;
-			break;
-		}
+		str[i] = ch;	// 입력한 문자를 str 배열에 저장
+		i++;			// 다음 문자 입력
 	}
+	
+	str[i] = '\0';		// 문자열을 완성하기 위해 마지막에 널문자 저장
 
-	for (int i = 0; i < stack.top; i++)
-	{
-		cout << stack.data[i] << " ";
-	}
+	cout << "입력된 문장은 : " << str << endl;
 
-	Delete(&stack);
+	// getline() 함수를 사용한 한 줄 입력
+
+	cout << "새로운 문장 입력 : ";
+	cin.getline(str, _countof(str));	// 한 줄의 데이터 입력
+	cout << "새로 입력된 문장 : " << str << endl;
 
 	return 0;
-}
-
-void Init(Stack* ps, int size)
-{
-	ps->size = size;
-	ps->data = (int*)malloc(size * sizeof(int));
-	ps->top = 0;
-}
-
-void Delete(Stack* ps)
-{
-	free(ps->data);
-}
-
-void Push(Stack* ps, int num)
-{
-	if (ps->top == ps->size)
-	{
-		printf("# 스택이 모두 찼습니다!\n");
-	}
-	else
-	{
-		ps->data[ps->top] = num;
-		ps->top++;
-	}
-}
-
-int Pop(Stack* ps)
-{
-	int num;
-
-	if (ps->top == 0)
-	{
-		printf("# 스택이 비었습니다!\n");
-		num = -1;
-	}
-	else
-	{
-		(ps->top)--;
-		num = ps->data[ps->top];
-	}
-
-	return num;
 }
