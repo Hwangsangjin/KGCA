73b06fa8f5ea1e21dca1fd947e25a232eb8f20f3
상진 <ghkdtkdwls88@gmail.com>
2022-07-18 02:53:20 +0900

    집에서 커밋

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 int main()
 {
-	vector<int> vec1{ 1, 2, 3, 4 };
-	cout << vec1.capacity() << ":" << vec1.size() << endl;
+	int n;
+	std::cout << "홀수 입력 : ";
+	std::cin >> n;
 
-	vec1.push_back(5);
-	cout << vec1.capacity() << ":" << vec1.size() << endl;
+	int arr[50][50] = { 0 };
+	int x = 0;
+	int y = (n - 1) / 2;
+	int end = n * n;
 
-	vec1.push_back(6);
-	cout << vec1.capacity() << ":" << vec1.size() << endl;
-
-	vec1.insert(vec1.begin(), 0);
-	cout << vec1.capacity() << ":" << vec1.size() << endl;
-
-	vec1.erase(vec1.begin() + 1, vec1.begin() + 3);
-	cout << vec1.capacity() << ":" << vec1.size() << endl;
-
-	for (size_t i = 0; i < vec1.size(); i++)
+	for (size_t i = 1; i <= end; i++)
 	{
-		cout << vec1[i] << ", ";
+		// 1. 시작은 첫 행, 한 가운데 열에 1을 둔다.
+		arr[x][y] = i;
+
+		// 2. 행을 감소, 열을 증가하면서 순차적으로 수를 넣어간다.
+		x--;
+		y++;
+
+		// 3. 행은 감소하므로 첫 행보다 작아지는 경우에는 마지막 행으로 넘어간다.
+		if (x < 0)
+		{
+			x = n - 1;
+		}
+
+		// 4. 열은 증가하므로 마지막 열보다 커지는 경우에는 첫 열로 넘어간다.
+		if (y > n - 1)
+		{
+			y = 0;
+		}
+
+		// 5. 넣은 수가 n의 배수이면 행만 증가한다. 열은 변화없음.
+		if (i % n == 0)
+		{
+			x++;
+		}
 	}
-	cout << endl;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
