70f82891b4d98432bfa9960238b1380cabf5904f
상진 <ghkdtkdwls88@gmail.com>
2022-07-28 03:59:11 +0900

    스택

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -22,35 +22,35 @@ public:
 
 // 노드 추가
 template <typename T>
-void Stack<T>::push(const T& e)
+inline void Stack<T>::push(const T& e)
 {
 	list.push_back(e);
 }
 
 // 노드 삭제
 template <typename T>
-void Stack<T>::pop()
+inline void Stack<T>::pop()
 {
 	list.pop_back();
 }
 
 // 마지막 노드의 데이터
 template <typename T>
-const T& Stack<T>::top() const
+inline const T& Stack<T>::top() const
 {
 	return list.back();
 }
 
 // 노드가 비었는지 확인
 template <typename T>
-bool Stack<T>::empty() const
+inline bool Stack<T>::empty() const
 {
 	return list.empty();
 }
 
 // 노드의 갯수
 template <typename T>
-int Stack<T>::size() const
+inline int Stack<T>::size() const
 {
 	return list.size();
 }
