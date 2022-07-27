8351629d17ab52b65fab16b2c58e6fc2cbda7192
상진 <ghkdtkdwls88@gmail.com>
2022-07-27 17:30:50 +0900

    연결 리스트

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -1,19 +1,19 @@
 #pragma once
 
-// 노드
-template <typename T>
-struct Node
-{
-	T data;
-	Node* prev;
-	Node* next;
-};
-
 // 연결 리스트
 template <typename T>
 class LinkedList
 {
 private:
+	// 노드
+	template <typename T>
+	struct Node
+	{
+		T data;
+		Node* prev;
+		Node* next;
+	};
+
 	// 반복자
 	class Iterator
 	{
@@ -91,9 +91,6 @@ public:
 		return Iterator(current);
 	}
 
-	void swap(Node<T>* a, Node<T>* b);
-	void sort();
-
 	void erase(const Iterator& iterator);
 	void pop_front();
 	void pop_back();
@@ -139,7 +136,7 @@ LinkedList<T>::~LinkedList()
 
 // 노드 삽입
 template <typename T>
-inline void LinkedList<T>::insert(const Iterator& iterator, const T& value)
+void LinkedList<T>::insert(const Iterator& iterator, const T& value)
 {
 	// 반복자의 위치를 노드에 전달
 	Node<T>* node = iterator.ptr;
@@ -158,7 +155,7 @@ inline void LinkedList<T>::insert(const Iterator& iterator, const T& value)
 
 // 노드를 앞에 추가
 template <typename T>
-inline void LinkedList<T>::push_front(const T& value)
+void LinkedList<T>::push_front(const T& value)
 {
 	// 머리의 다음 위치를 전달
 	insert(begin(), value);
@@ -166,22 +163,15 @@ inline void LinkedList<T>::push_front(const T& value)
 
 // 노드를 뒤에 추가
 template <typename T>
-inline void LinkedList<T>::push_back(const T& value)
+void LinkedList<T>::push_back(const T& value)
 {
 	// 꼬리의 위치를 전달
 	insert(end(), value);
 }
 
-// 노드 정렬
-template<typename T>
-inline void LinkedList<T>::sort()
-{
-	
-}
-
 // 노드 삭제
 template<typename T>
-inline void LinkedList<T>::erase(const Iterator& iterator)
+void LinkedList<T>::erase(const Iterator& iterator)
 {
 	// 반복자의 위치를 노드에 전달
 	Node<T>* node = iterator.ptr;
@@ -199,7 +189,7 @@ inline void LinkedList<T>::erase(const Iterator& iterator)
 
 // 가장 앞의 노드를 삭제
 template<typename T>
-inline void LinkedList<T>::pop_front()
+void LinkedList<T>::pop_front()
 {
 	// 노드가 비어있지 않다면
 	if (!empty())
@@ -211,7 +201,7 @@ inline void LinkedList<T>::pop_front()
 
 // 가장 뒤의 노드를 삭제
 template<typename T>
-inline void LinkedList<T>::pop_back()
+void LinkedList<T>::pop_back()
 {
 	// 노드가 비어있지 않다면
 	if (!empty())
@@ -223,7 +213,7 @@ inline void LinkedList<T>::pop_back()
 
 // 모든 노드 삭제
 template<typename T>
-inline void LinkedList<T>::clear()
+void LinkedList<T>::clear()
 {
 	// 삭제할 임시 노드 생성
 	Node<T>* temp = nullptr;
@@ -252,14 +242,14 @@ inline void LinkedList<T>::clear()
 
 // 노드가 비었는지 확인
 template<typename T>
-inline bool LinkedList<T>::empty() const
+bool LinkedList<T>::empty() const
 {
 	return count == 0;
 }
 
 // 노드의 갯수
 template<typename T>
-inline int LinkedList<T>::size() const
+int LinkedList<T>::size() const
 {
 	return count;
 }
