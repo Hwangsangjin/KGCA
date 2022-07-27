d76ad2bfc24fb007c4b40d76434df58e56a0d72c
상진 <ghkdtkdwls88@gmail.com>
2022-07-27 22:51:03 +0900

    학원에서 커밋

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,69 +1,26 @@
 #include <iostream>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-void bubble_sort(int data[], int n)
-{
-	for (size_t i = 0; i < n - 1; i++)
-	{
-		for (size_t j = n - 1; j > i; j--)
-		{
-			if (data[j] < data[j - 1])
-			{
-				swap(data[j], data[j - 1]);
-			}
-		}
-	}
-}
-
-void selection_sort(int data[], int n)
-{
-	for (size_t i = 0; i < n - 1; i++)
-	{
-		int idx = i;
-
-		for (size_t j = i; j < n; j++)
-		{
-			if (data[j] < data[idx])
-			{
-				idx = j;
-			}
-		}
-
-		swap(data[i], data[idx]);
-	}
-}
-
-void insertion_sort(int data[], int n)
-{
-	for (size_t i = 1; i < n; i++)
-	{
-		int key = data[i];
-		int j = i - 1;
-
-		while (j >= 0 && data[j] > key)
-		{
-			data[j + 1] = data[j];
-			j--;
-		}
-
-		data[j + 1] = key;
-	}
-}
-
 int main()
 {
-	int data[] = { 4, 2, 3, 5, 1 };
-	bubble_sort(data, 5);
-	selection_sort(data, 5);
-	insertion_sort(data, 5);
+	queue<int> q;
+	q.push(10);
+	q.push(20);
+	q.push(30);
+	q.pop();
 
-	for (auto& i : data)
+	cout << q.front() << endl;
+	q.push(40);
+
+	while (!q.empty())
 	{
-		cout << i << " ";
+		auto& e = q.front();
+		cout << e << " ";
+		q.pop();
 	}
 	cout << endl;
-
+	
 	return 0;
 }
