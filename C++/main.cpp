2116909615960af41c5ffd7eb9c8232f3ee285ed
상진 <ghkdtkdwls88@gmail.com>
2022-07-29 18:40:33 +0900

    학원에서 커밋

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,31 +1,74 @@
 #include <iostream>
-#include <Queue.h>
-
-using namespace std;
+#include <set>
+#include <map>
 
 int main()
 {
-	CircularQueue<int> q(5);
-	
-	q.enqueue(10);
-	q.enqueue(20);
-	q.enqueue(30);
-	q.enqueue(40);
-	q.enqueue(50);
-	q.dequeue();
-	q.dequeue();
-	q.enqueue(60);
-	q.enqueue(70);
+    std::set<int> odds{ 1, 7, 5, 3, 9 };
+    std::set<int, std::greater<>> evens{ 2, 4, 6, 8 };
+    evens.insert(10);
+    evens.insert(2);
 
-	q.enqueue(80);
-	
-	while (!q.empty())
-	{
-		auto& e = q.front();
-		cout << e << " ";
-		q.dequeue();
-	}
-	cout << endl;
+    if (evens.find(4) != evens.end())
+    {
+        std::cout << "4 is found!" << std::endl;
+    }
+    else
+    {
+        std::cout << "4 is not found!" << std::endl;
+    }
 
-	return 0;
+    for (auto& n : odds)
+    {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+    for (auto& n : evens)
+    {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+    using psi = std::pair<std::string, int>;
+    std::set<psi> managers{ {"Amelia", 29}, {"Noah", 25}, {"Olivia", 31}, {"Sophia", 40} };
+    managers.insert({ "George", 35 });
+
+    psi person1 = { "Noah", 25 };
+
+    if (managers.find(person1) != managers.end())
+    {
+        std::cout << person1.first << " is a manager!" << std::endl;
+    }
+    else
+    {
+        std::cout << person1.first << " is not a manager!" << std::endl;
+    }
+
+    managers.erase({ "Sophia", 40 });
+    managers.erase({ "Noah", 40 });
+
+    for (auto& m : managers)
+    {
+        std::cout << m.first << "(" << m.second << ")" << std::endl;
+    }
+
+    std::map<std::string, int> fruits;
+    fruits.insert({ "apple", 1000 });
+    fruits.insert({ "banana", 1500 });
+    fruits["orange"] = 3000;
+    fruits["grape"] = 4000;
+    fruits["lemon"] = 5000;
+
+    fruits["apple"] = 2000;
+    fruits.erase("grape");
+
+    std::cout << fruits["apple"] << std::endl;
+
+    for (auto& p : fruits)
+    {
+        std::cout << p.first << " is " << p.second << " won." << std::endl;
+    }
+
+    return 0;
 }
