4108f810de0da1b6ab2402bf92fc55b31f5758ec
상진 <ghkdtkdwls88@gmail.com>
2022-08-01 22:11:53 +0900

    학원에서 커밋

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,30 +1,59 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int score;
+
+    bool operator<(const Student& st) const
+    {
+        return score < st.score;
+    }
+};
+
+template <typename T>
+void Print_queue(T q)
+{
+    while (!q.empty())
+    {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
 
 int main()
 {
-    int aList[5] = { 10, 20, 30, 40, 50 };
+    std::vector<int> vec{ 4, 2, 3, 5, 1 };
 
-    // 전형적인 C 스타일 반복문
-    for (size_t i = 0; i < 5; i++)
+    priority_queue<int> pq1;
+
+    for (auto& n : vec)
     {
-        std::cout << aList[i] << ' ';
+        pq1.push(n);
     }
-    std::cout << std::endl;
+    Print_queue(pq1);
 
-    // 범위 기반 C++11 스타일 반복문
-    // 각 요소의 값을 n에 복사한다.
-    for (auto n : aList)
+    priority_queue<int, vector<int>, greater<int>> pq2(vec.begin(), vec.end());
+    Print_queue(pq2);
+
+    priority_queue<Student> students;
+    students.push({ "Amelia", 80 });
+    students.push({ "Sophia", 40 });
+    students.push({ "Olivia", 95 });
+    students.push({ "George", 70 });
+
+    while (!students.empty())
     {
-        std::cout << n << std::endl;
+        auto& s = students.top();
+        cout << s.name << " (" << s.score << ")" << endl;
+        students.pop();
     }
-    std::cout << std::endl;
-
-    // n은 각 요소에 대한 참조다.
-    for (auto& n : aList)
-    {
-        std::cout << n << ' ';
-    }
-    std::cout << std::endl;
-
+    
     return 0;
 }
