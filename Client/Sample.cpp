40bc83cbb665b4cbe279c8d1a8e9492472c857bb
상진 <ghkdtkdwls88@gmail.com>
2022-09-10 01:16:19 +0900

    클라이언트

--- Client/Sample.cpp
+++ Client/Sample.cpp
@@ -19,28 +19,30 @@ HRESULT Sample::Render()
 {
 	if (INPUT->GetKey(VK_LBUTTON) == KEY_STATE::HOLD)
 	{
-		_font.Draw(pos.x - 20, pos.y - 20, L"MOUSE", { 1, 1, 1, 1 });
+		_font.Draw(pos.x, pos.y - 20, L"MOUSE", { 1, 1, 1, 1 });
+	}
+
+
+	if (INPUT->GetKey('W') == KEY_STATE::HOLD)
+	{
+		_font.Draw(50, 150, L"W", { 1, 1, 1, 1 });
+	}
+	if (INPUT->GetKey('A') == KEY_STATE::HOLD)
+	{
+		_font.Draw(30, 170, L"A", { 1, 1, 1, 1 });
+	}
+	if (INPUT->GetKey('S') == KEY_STATE::HOLD)
+	{
+		_font.Draw(50, 170, L"S", { 1, 1, 1, 1 });
+	}
+	if (INPUT->GetKey('D') == KEY_STATE::HOLD)
+	{
+		_font.Draw(70, 170, L"D", { 1, 1, 1, 1 });
 	}
 
 	if (INPUT->GetKey(VK_SPACE) == KEY_STATE::HOLD)
 	{
-		_font.Draw(rtClient.right / 2 - 20, rtClient.bottom / 2 - 20, L"SPACE", { 1, 1, 1, 1 });
-	}
-	if (INPUT->GetKey('W') == KEY_STATE::HOLD)
-	{
-		_font.Draw(100, 100, L"W", { 1, 1, 1, 1 });
-	}
-	if (INPUT->GetKey('A') == KEY_STATE::HOLD)
-	{
-		_font.Draw(100, 130, L"A", { 1, 1, 1, 1 });
-	}
-	if (INPUT->GetKey('S') == KEY_STATE::HOLD)
-	{
-		_font.Draw(100, 160, L"S", { 1, 1, 1, 1 });
-	}
-	if (INPUT->GetKey('D') == KEY_STATE::HOLD)
-	{
-		_font.Draw(100, 190, L"D", { 1, 1, 1, 1 });
+		_font.Draw(30, 280, L"SPACE", { 1, 1, 1, 1 });
 	}
 
     return TRUE;
