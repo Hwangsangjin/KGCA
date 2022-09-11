bb4285561df4c9d7fad725071b6fc60bc4b24bef
상진 <ghkdtkdwls88@gmail.com>
2022-09-11 23:24:30 +0900

    텍스처 생성

--- CreateTexture/BaseObject.cpp
+++ CreateTexture/BaseObject.cpp
@@ -63,23 +63,18 @@ HRESULT BaseObject::CreateVertexBuffer()
     _vertices[0].position = Vector3{ -0.1f, 0.2f, 0.0f };
     _vertices[0].color = Vector4{ 0.0f, 0.0f, 1.0f, 1.0f };
     _vertices[0].uv = Vector2{ 0.0f, 0.0f };
-
     _vertices[1].position = Vector3{ 0.1f, 0.2f , 0.0f };
     _vertices[1].color = Vector4{ 0.0f, 1.0f, 0.0f, 1.0f };
     _vertices[1].uv = Vector2{ 1.0f, 0.0f };
-
     _vertices[2].position = Vector3{ -0.1f, -0.2f, 0.0f };
     _vertices[2].color = Vector4{ 1.0f, 0.0f, 1.0f, 1.0f };
     _vertices[2].uv = Vector2{ 0.0f, 1.0f };
-
     _vertices[3].position = Vector3{ -0.1f, -0.2f, 0.0f };
     _vertices[3].color = Vector4{ 1.0f, 0.0f, 1.0f, 1.0f };
     _vertices[3].uv = Vector2{ 0.0f, 1.0f };
-
     _vertices[4].position = Vector3{ 0.1f, 0.2f, 0.0f };
     _vertices[4].color = Vector4{ 0.0f, 1.0f, 0.0f, 1.0f };
     _vertices[4].uv = Vector2{ 1.0f, 0.0f };
-
     _vertices[5].position = Vector3{ 0.1f, -0.2f, 0.0f };
     _vertices[5].color = Vector4{ 1.0f, 1.0f, 0.0f, 1.0f };
     _vertices[5].uv = Vector2{ 1.0f, 1.0f };
