872ee88ac9388a0194114edcafcc063176a1d498
상진 <ghkdtkdwls88@gmail.com>
2022-10-18 05:17:50 +0900

    엔진

--- Engine/Object.cpp
+++ Engine/Object.cpp
@@ -112,16 +112,16 @@ void Object::CreateVertexData()
 {
     _vertices.resize(4);
 
-    _vertices[0].position = MyVector3{ -1.0f, 1.0f, -1.0f };
+    _vertices[0].position = MyVector3{ -1.0f, 1.0f, 0.0f };
     _vertices[0].color = Vector4{ 0.0f, 0.0f, 1.0f, 1.0f };
     _vertices[0].uv = MyVector2{ 0.0f, 0.0f };
-    _vertices[1].position = MyVector3{ 1.0f, 1.0f , -1.0f };
+    _vertices[1].position = MyVector3{ 1.0f, 1.0f , 0.0f };
     _vertices[1].color = Vector4{ 0.0f, 1.0f, 0.0f, 1.0f };
     _vertices[1].uv = MyVector2{ 1.0f, 0.0f };
-    _vertices[2].position = MyVector3{ 1.0f, -1.0f, -1.0f };
+    _vertices[2].position = MyVector3{ 1.0f, -1.0f, 0.0f };
     _vertices[2].color = Vector4{ 1.0f, 0.0f, 1.0f, 1.0f };
     _vertices[2].uv = MyVector2{ 1.0f, 1.0f };
-    _vertices[3].position = MyVector3{ -1.0f, -1.0f, -1.0f };
+    _vertices[3].position = MyVector3{ -1.0f, -1.0f, 0.0f };
     _vertices[3].color = Vector4{ 1.0f, 1.0f, 0.0f, 1.0f };
     _vertices[3].uv = MyVector2{ 0.0f, 1.0f };
 
@@ -152,14 +152,10 @@ HRESULT Object::CreateVertexBuffer()
 
 void Object::CreateIndexData()
 {
-    // 정점 버퍼에 인덱스
     _indices.resize(6);
-    _indices[0] = 0;
-    _indices[1] = 1;
-    _indices[2] = 2;
-    _indices[3] = 0;
-    _indices[4] = 2;
-    _indices[5] = 3;
+
+    _indices[0] = 0; _indices[1] = 2; _indices[2] = 3;
+    _indices[3] = 0; _indices[4] = 1; _indices[5] = 3;
 }
 
 HRESULT Object::CreateIndexBuffer()
