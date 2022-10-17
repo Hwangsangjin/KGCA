287cdcb6498fe23ff7d925a443e6fd6b15924450
상진 <ghkdtkdwls88@gmail.com>
2022-10-18 05:43:01 +0900

    카메라 생성

--- CreateCamera/Scene.cpp
+++ CreateCamera/Scene.cpp
@@ -4,64 +4,55 @@
 
 HRESULT Scene::Init()
 {
-	// 텍스처
-	TEXTURE->Load(L"../../../Resource/Ryan/Ryan.png");
-	TEXTURE->Load(L"../../../Resource/Box/Box.png");
+	// 카메라
+	_pMainCamera = new Camera;
+	_pMainCamera->CreateView(MyVector3(0, 0, -5), MyVector3(0, 0, 0), MyVector3(0, 1, 0));
+	_pMainCamera->CreateProjection(1.0f, 100.0f, PIDIV2, RESOLUTION_RATIO);
 
 	// 라이언
 	_pRyan = new Actor;
 	_pRyan->CreateObject(_pd3dDevice, _pImmediateContext, L"../../../Resource/Shader/Default.hlsl", L"../../../Resource/Ryan/Ryan.png");
+	AddObject(_pRyan);
 
 	// 박스
 	_pBox = new ShapeBox;
 	_pBox->CreateObject(_pd3dDevice, _pImmediateContext, L"../../../Resource/Shader/Default.hlsl", L"../../../Resource/Box/Box.png");
-
-	// 카메라
-	_pMainCamera = new Camera;
-	_pMainCamera->CreateView(MyVector3(0, 0, -10), MyVector3(0, 0, 0), MyVector3(0, 1, 0));
-	_pMainCamera->CreateProjection(1.0f, 100.0f, 3.141592 * 0.5f, RESOLUTION_X / RESOLUTION_Y);
+	AddObject(_pBox);
 
 	return TRUE;
 }
 
 HRESULT Scene::Frame()
 {
-	_pRyan->Frame();
-	_pBox->Frame();
 	_pMainCamera->Frame();
 
-	//for (auto& pObject : _pObjects)
-	//{
-	//	pObject->Frame();
-	//}
+	for (auto& pObject : _pObjects)
+	{
+		pObject->Frame();
+	}
 
 	return TRUE;
 }
 
 HRESULT Scene::Render()
 {
-	_pRyan->SetMatrix(nullptr, &_pMainCamera->_View, &_pMainCamera->_Projection);
-	//_pRyan->Render();
-
-	_pBox->SetMatrix(nullptr, &_pMainCamera->_View, &_pMainCamera->_Projection);
-	_pBox->Render();
-
-	/*for (auto& pObject : _pObjects)
+	for (auto& pObject : _pObjects)
 	{
+		_pRyan->SetMatrix(nullptr, &_pMainCamera->_View, &_pMainCamera->_Projection);
+		_pBox->SetMatrix(nullptr, &_pMainCamera->_View, &_pMainCamera->_Projection);
+
 		pObject->Render();
-	}*/
+	}
 
 	return TRUE;
 }
 
 HRESULT Scene::Release()
 {
-	SAFE_DELETE(_pBox);
-
-	//for (auto& pObject : _pObjects)
-	//{
-	//	SAFE_RELEASE(pObject);
-	//}
+	for (auto& pObject : _pObjects)
+	{
+		SAFE_DELETE(pObject);
+	}
 
 	return TRUE;
 }
