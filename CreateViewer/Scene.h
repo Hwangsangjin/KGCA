a6174dd935d8c638d80ea6bcd8a42aab3050da94
상진 <ghkdtkdwls88@gmail.com>
2022-10-20 23:06:05 +0900

    뷰어 생성

---
+++ CreateViewer/Scene.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class Scene
+{
+public:
+	// 초기화
+	virtual HRESULT Init();
+	// 프레임 계산
+	virtual HRESULT Frame();
+	// 렌더
+	virtual HRESULT Render();
+	// 메모리 해제
+	virtual HRESULT Release();
+
+	void CreateScene(ID3D11Device* pd3dDevice, ID3D11DeviceContext* pImmedtateContext);
+	void AddObject(class Object* pObject);
+
+private:
+	ID3D11Device* _pd3dDevice = nullptr;
+	ID3D11DeviceContext* _pImmediateContext = nullptr;
+
+	class Camera* _pMainCamera = nullptr;
+	class Map* _pMap = nullptr;
+	class Actor* _pRyan = nullptr;
+	class Box* _pBox = nullptr;
+	class Box* _pBox2 = nullptr;
+
+	std::vector<Object*> _pObjects;
+};
+
