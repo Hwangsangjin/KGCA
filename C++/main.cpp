7667aada38038b54bd998985333dd70a46436856
상진 <ghkdtkdwls88@gmail.com>
2022-11-26 16:37:15 +0900

    C++ 프로그래밍

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
-
-// string 클래스를 사용하기 위한 헤더
-#include <string>
-
-using namespace std;
+#include <vector>
 
 int main()
 {
-    string strName;
-    cout << "이름: ";
-    cin >> strName;
-    cout << "당신의 이름은 " + strName + "입니다." << endl;
+    // 요소가 int 자료형이고 개수가 세 개인 벡터 선언
+    std::vector<int> vec(3);
+    vec[0] = 10;
+    vec[1] = 20;
+    vec[2] = 30;
+
+    for (auto& i : vec)
+    {
+        std::cout << i << '\t';
+    }
+
+    std::cout << std::endl;
+
+    // 뒤에 새로운 요소 두 개를 더 추가한다.
+    vec.push_back(30);
+    vec.push_back(40);
+
+    for (auto& i : vec)
+    {
+        std::cout << i << '\t';
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
